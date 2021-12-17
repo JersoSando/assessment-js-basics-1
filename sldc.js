807be36e2557d054d135786a470e9a1d5885cf54
Jerso Sando <jerso.sando@gmail.com>
2021-12-17 09:53:25 -0700

    first file

--- sldc.js
+++ sldc.js
@@ -12,6 +12,13 @@
 // Planning
 // Testing & Integration
 
+Planning
+Analysis of requirements
+Design
+Implementation
+Testing & integration
+Maintain 
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +26,13 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "This is the second step, in which developers will look at the requirements being asked and think about or understand what you want your project to do "
+
+const design = "This is the third step. This is where developers will draw out what they want it to look like"
+
+const implementation = "This is the fourth step. During this step, developers will code out their project"
+
+const testingAndIntegration = "This is the fifth step. Developers will make sure their code is working and then send it out to others"
+
+const maintain = "This is the sixth step. Once your code is sent out, developers have to make sure it stays working correctly"
